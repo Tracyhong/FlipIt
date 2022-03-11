a25389d5b333727b902b2d12cc13171026d80ca1
Tracyhong <72313520+Tracyhong@users.noreply.github.com>
2022-03-11 12:12:19 +0100

    splash screen

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import 'react-native-gesture-handler';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>FLIP !T !</Text>
       <StatusBar style="auto" />
     </View>
   );
