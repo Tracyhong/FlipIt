c7ab3d02605fe22133324ffaa91e3adb26c56fac
Tracyhong <72313520+Tracyhong@users.noreply.github.com>
2022-03-22 01:00:32 +0100

    add connexion

--- components/connexion.js
+++ components/connexion.js
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image,TextInput,TouchableOpacity } from 'react-native';
 import 'react-native-gesture-handler';
-
+/*import {useState} from 'react';
+const [email, setEmail ] = useState('');
+const [password , setPassword ] = useState( '' ) ;*/
 export default function Connexion() {
   return (
     <View style={styles.container}>
-      <Text>Connexion</Text>
+      <Image
+          style={styles.tinyLogo}
+          source={require('../assets/Flip!t.png')}
+      />
+      <TextInput style={styles.input}
+        editable
+        maxLength={50}
+        //leftIcon='email'  //je sais pas ca sert a quoi
+        placeholder='Enter email'
+        autoCapitalize='none'
+        keybordType='email-address'
+        textContentType='emailAddress'
+        autoFocus={true}
+        //value={email}
+        //onChangeText={(email) => setEmail(email)}
+
+      />
+      <TextInput style={styles.input}
+        editable
+        maxLength={50}
+        //leftIcon='lock' //je sais pas ca sert a quoi
+        placeholder='Enter password'
+        autoCapitalize='none'
+        autoCorrect={false}
+        textContentType='password'
+        //rightIcon={rightIcon} //je sais pas ca sert a quoi
+
+        secureTextEntry={true}
+
+        //value={password}
+        //onChangeText={(password) => setPassword(password)}
+      />
+
+      <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+          <Text style={styles.signup_button}>Sign up ?</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +58,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tinyLogo: {
+    width: 110,
+      height: 60,
+  },
+  input: {
+      height: 40,
+      width:'80%',
+      margin: 12,
+      borderWidth: 1,
+      borderColor:'#BFBFC1',
+      backgroundColor:'#FBFBFB',
+      borderRadius:15,
+      padding: 10,
+  },
+  signup_button: {
+      height: 30,
+      marginTop: 30,
+  },
+  loginBtn:{
+      height: 40,
+      width:'80%',
+      borderRadius:25,
+      alignItems:"center",
+      justifyContent:"center",
+      backgroundColor:'#00A2E8',
+      color:'#fff',
+      marginBottom: 30,
+      marginTop: 30,
+  }
 });
