fd01c8e353bffec232bf73ee20e707b81ebe8fa3
Lorie <83126685+Khaalyx@users.noreply.github.com>
2022-03-26 17:43:13 +0100

    change firebase

--- firebase.js
+++ firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAP7hGZ-F85VqJd-wVI_1X4p8-AwmgMNjE",
-  authDomain: "flip-t.firebaseapp.com",
-  projectId: "flip-t",
-  storageBucket: "flip-t.appspot.com",
-  messagingSenderId: "1030659244307",
-  appId: "1:1030659244307:web:a02afb9b72173c1e46dae6"
+  apiKey: "AIzaSyC0idYaBM9LsP4OrxWvfUB_e1EoEn8TEDo",
+  authDomain: "flipit-39bc4.firebaseapp.com",
+  projectId: "flipit-39bc4",
+  storageBucket: "flipit-39bc4.appspot.com",
+  messagingSenderId: "389891114397",
+  appId: "1:389891114397:web:0b29a68a67e58020261b74"
 };
 
 // Initialize Firebase
