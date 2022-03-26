5fb379d20f1e93121b40b9a0a92180745eacab3b
Lorie <83126685+Khaalyx@users.noreply.github.com>
2022-03-26 14:18:34 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,3 @@
-import { createStackNavigator } from "react-navigation-stack";
-import { createAppContainer } from 'react-navigation';
 import Accueil from '../components/accueil';
 import Profil from '../components/profil';
 import Connexion from '../components/connexion';
@@ -80,36 +78,3 @@ export default function Index() {
     </NavigationContainer>
   );
 }
-
-// const screens = {
-//     Connexion: {
-//         screen: Connexion,
-//         navigationOptions: { //pour enlever le header
-//            //header: null,
-//            headerShown:false
-//           },
-//     },
-//     ListDeck:{
-//         screen: ListDeck,
-//         navigationOptions: {
-//             // headerTitle: (
-//             //     <Image style={{height:45, width:82, margin:'27%' }} source={require('../assets/Flip!t.png')}/>
-//             // ),
-//             headerTitle: () => <Image style={{height:45, width:82, margin:'27%' }} source={require('../assets/Flip!t.png')}/>
-//           }
-//     },
-//     Deck:{
-//         screen:Deck
-//     },
-    
-//     Accueil: {
-//         screen: Accueil
-//     },
-//     Profil: {
-//         screen: Profil
-//     },
-// }
-
-// const HomeStack = createStackNavigator(screens);
-
-// export default createAppContainer(HomeStack);
